b62ff21038ca577b49409350ca5ca3616e79d727
William Hayes <william.s.hayes@gmail.com>
2018-01-02 12:08:53 -0500

    Commit example tests

---
+++ tests/test_config.py
@@ -0,0 +1,17 @@
+import bel.Config as Config
+
+
+def test_config():
+
+    config = Config.load_configuration()
+    assert config['bel']['version'] >= '0.5.0'
+
+
+def test_merge_config():
+
+    config = Config.load_configuration()
+    override_config = {'bel_api': {'servers': {'server_type': 'DEV2'}}}
+    new_config = Config.merge_config(config, override_config=override_config)
+
+    assert config['bel_api']['servers']['server_type'] == 'DEV'
+    assert new_config['bel_api']['servers']['server_type'] == 'DEV2'
---
+++ tests/test_utils.py
@@ -0,0 +1,32 @@
+import re
+import bel.utils as utils
+
+
+def test_first_true():
+
+    test1 = [False, 1, '2', None]
+    test2 = [None, '', '2', None]
+    test3 = [None, False, '']
+
+    result = utils.first_true(test1)
+    assert result == 1
+
+    result = utils.first_true(test2)
+    assert result == '2'
+
+    # Result is the default value '3'
+    result = utils.first_true(test3, '3')
+    assert result == '3'
+
+
+def test_create_hash():
+
+    h = utils._create_hash('test')
+    assert h == 204797213367049729698754624420042367389
+
+
+def test_generate_id():
+
+    _id = utils._generate_id()
+    assert re.match('\w{26,26}', str(_id))
+
