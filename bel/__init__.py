a7962978e43c28629020ee402128a256931bc8e9
William Hayes <william.s.hayes@gmail.com>
2018-07-25 14:51:28 -0400

    default imports

--- bel/__init__.py
+++ bel/__init__.py
@@ -2,8 +2,14 @@
 # BEL FUNCTION IMPORTS #
 ########################
 
-import bel.lang.bel_specification as bel_specification
+import bel.db
+import bel.edge
+import bel.lang
+import bel.nanopub
+import bel.resources
+
 from bel.lang.belobj import BEL
+import bel.lang.bel_specification as bel_specification
 
 import logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
