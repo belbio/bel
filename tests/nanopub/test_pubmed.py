c50553d2b6f7889d4cd35e462257d19e04e02336
William Hayes <william.s.hayes@gmail.com>
2019-04-09 09:32:03 -0400

    Fixed pubmed abstract issue

--- tests/nanopub/test_pubmed.py
+++ tests/nanopub/test_pubmed.py
@@ -2,16 +2,39 @@ import bel.nanopub.pubmed
 import json
 
 
-def test_get_pubmed():
+def test_get_pubmed1():
     """Get a normal Pubmed record"""
 
-    pmid = '10551823'
+    pmid = "10551823"
 
     doc = bel.nanopub.pubmed.get_pubmed(pmid)
 
-    print('Doc:\n', json.dumps(doc, indent=4))
+    print("Doc:\n", json.dumps(doc, indent=4))
 
-    assert doc['pmid'] == pmid
+    assert doc["pmid"] == pmid
+
+
+def test_get_pubmed2():
+    """Get a normal Pubmed record"""
+
+    # TODO https://stackoverflow.com/questions/4770191/lxml-etree-element-text-doesnt-return-the-entire-text-from-an-element
+    #  https://www.ncbi.nlm.nih.gov/pubmed/?term=27822475&report=xml&format=text
+    #   <Abstract>
+    #     <AbstractText>Rheumatoid arthritis (RA) appears as inflammation of synovial tissue and joint destruction. Receptor activator of NF-<i>κ</i>B (RANK) is a member of the TNF receptor superfamily and a receptor for the RANK ligand (RANKL). In this study, we examined the expression of RANK<sup>high</sup> and CCR6 on CD14<sup>+</sup> monocytes from patients with RA and healthy volunteers. Peripheral blood samples were obtained from both the RA patients and the healthy volunteers. Osteoclastogenesis from monocytes was induced by RANKL and M-CSF <i>in vitro</i>. To study the expression of RANK<sup>high</sup> and CCR6 on CD14<sup>+</sup> monocytes, two-color flow cytometry was performed. Levels of expression of RANK on monocytes were significantly correlated with the level of osteoclastogenesis in the healthy volunteers. The expression of RANK<sup>high</sup> on CD14<sup>+</sup> monocyte in RA patients without treatment was elevated and that in those receiving treatment was decreased. In addition, the high-level expression of RANK on CD14<sup>+</sup> monocytes was correlated with the high-level expression of CCR6 in healthy volunteers. Monocytes expressing both RANK and CCR6 differentiate into osteoclasts. The expression of CD14<sup>+</sup>RANK<sup>high</sup> in untreated RA patients was elevated. RANK and CCR6 expressed on monocytes may be novel targets for the regulation of bone resorption in RA and osteoporosis.</AbstractText>
+    #   </Abstract>
+    #   stops abstract at first subtag  ...NF-<i>
+    #
+    pmid = "27822475"
+
+    doc = bel.nanopub.pubmed.get_pubmed(pmid)
+
+    print("Doc:\n", json.dumps(doc, indent=4))
+
+    assert doc["pmid"] == pmid
+    assert (
+        doc["abstract"]
+        == "Rheumatoid arthritis (RA) appears as inflammation of synovial tissue and joint destruction. Receptor activator of NF-B (RANK) is a member of the TNF receptor superfamily and a receptor for the RANK ligand (RANKL). In this study, we examined the expression of RANK and CCR6 on CD14 monocytes from patients with RA and healthy volunteers. Peripheral blood samples were obtained from both the RA patients and the healthy volunteers. Osteoclastogenesis from monocytes was induced by RANKL and M-CSF . To study the expression of RANK and CCR6 on CD14 monocytes, two-color flow cytometry was performed. Levels of expression of RANK on monocytes were significantly correlated with the level of osteoclastogenesis in the healthy volunteers. The expression of RANK on CD14 monocyte in RA patients without treatment was elevated and that in those receiving treatment was decreased. In addition, the high-level expression of RANK on CD14 monocytes was correlated with the high-level expression of CCR6 in healthy volunteers. Monocytes expressing both RANK and CCR6 differentiate into osteoclasts. The expression of CD14RANK in untreated RA patients was elevated. RANK and CCR6 expressed on monocytes may be novel targets for the regulation of bone resorption in RA and osteoporosis."
+    )
 
 
 def test_get_pubmed_structured_abstract():
@@ -20,11 +43,11 @@ def test_get_pubmed_structured_abstract():
     Structured abstracts are stored differently than regular abstracts
     """
 
-    pmid = '30562878'
+    pmid = "30562878"
 
     doc = bel.nanopub.pubmed.get_pubmed(pmid)
 
-    print('Doc:\n', json.dumps(doc, indent=4))
+    print("Doc:\n", json.dumps(doc, indent=4))
 
-    assert doc['pmid'] == pmid
-    assert "Subjects and Methods" in doc['abstract']
+    assert doc["pmid"] == pmid
+    assert "Subjects and Methods" in doc["abstract"]
