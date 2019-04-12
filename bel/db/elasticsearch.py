30f11f085122fb905de13ad5f64b13bd56347ae3
Ben Aaron <ben@theaarons.com>
2019-04-12 14:27:51 -0700

    call full loader when loading BEL specs from yaml

--- bel/db/elasticsearch.py
+++ bel/db/elasticsearch.py
@@ -51,7 +51,7 @@ def create_terms_index(es, index_name: str):
     """Create terms index"""
 
     with open(mappings_terms_fn, 'r') as f:
-        mappings_terms = yaml.load(f)
+        mappings_terms = yaml.load(f, Loader=yaml.FullLoader)
 
     try:
         es.indices.create(index=index_name, body=mappings_terms)
--- bel/lang/bel_specification.py
+++ bel/lang/bel_specification.py
@@ -250,7 +250,7 @@ def belspec_yaml2json(yaml_fn: str, json_fn: str) -> str:
     """
 
     try:
-        spec_dict = yaml.load(open(yaml_fn, 'r').read())
+        spec_dict = yaml.load(open(yaml_fn, 'r').read(), Loader=yaml.FullLoader)
 
         # admin-related keys
         spec_dict['admin'] = {}
@@ -567,7 +567,7 @@ def create_ebnf_parser(files):
         ebnf_fn = belspec_fn.replace('.yaml', '.ebnf')
         if not os.path.exists(ebnf_fn) or os.path.getmtime(belspec_fn) > os.path.getmtime(ebnf_fn):
             with open(belspec_fn, 'r') as f:
-                belspec = yaml.load(f)
+                belspec = yaml.load(f, Loader=yaml.FullLoader)
 
             tmpl_dir = os.path.dirname(tmpl_fn)
             tmpl_basename = os.path.basename(tmpl_fn)
